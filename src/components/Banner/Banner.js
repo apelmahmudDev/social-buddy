e36c28b68bf4b861fbbd41ca0a05a9f40839fb9f
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-09-18 12:28:47 +0800

    social buddy is almost done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Banner.css';
+import banner from '../../images/banner2.jpg';
+import { Box, Button, Container, Typography } from '@material-ui/core';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
 
 const Banner = () => {
     return (
         <div className="banner">
-            
+            <div>
+                <img src={banner} alt=""/>
+            </div>
+            <Container className="banner__text">
+                <Typography variant="h4" className="banner__text__title">
+                    We are <br/> Friends all here....
+                </Typography>
+                <Box mt={2}>
+                    <Button variant="contained"><GroupAddIcon style={{marginRight:'5px'}} /> Connect with us</Button>
+                </Box>
+            </Container>
         </div>
     );
 };
